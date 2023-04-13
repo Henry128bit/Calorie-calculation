0e8b050d7f1c7ccc9eb074b04507087f3bf6a8bb
Sergey Kamensky <henry.128bit@gmail.com>
2023-04-13 22:47:46 +0400

    Hot Fix

--- webpack.config.js
+++ webpack.config.js
@@ -33,7 +33,7 @@ module.exports = {
     },
     plugins: [
       new CopyWebpackPlugin({
-        patterns: [{ from: 'public' }]
+        patterns: [{ from: 'public', to: 'public'}]
       })
     ]
 };
